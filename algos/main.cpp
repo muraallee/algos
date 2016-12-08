1dcbcd64fe15e5243bb96aae72dfbeacc5d70777
Murali Vallimanalan <muravallimanalan@Muralis-MacBook-Pro.local>
2016-12-08 16:52:23 +0530

    Initial Commit

---
+++ algos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algos
+//
+//  Created by Murali Vallimanalan on 08/12/16.
+//  Copyright © 2016 murav. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
