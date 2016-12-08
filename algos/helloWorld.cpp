648ed530e33e4ba19cfc234b94e9ffc355ef2bbf
Murali Vallimanalan <muravallimanalan@Muralis-MacBook-Pro.local>
2016-12-08 17:04:22 +0530

    With hello world

---
+++ algos/helloWorld.cpp
@@ -0,0 +1,16 @@
+//
+//  helloWorld.cpp
+//  algos
+//
+//  Created by Murali Vallimanalan on 08/12/16.
+//  Copyright © 2016 murav. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    
+    cout << "Hello World... This is the start..." << endl;
+}
